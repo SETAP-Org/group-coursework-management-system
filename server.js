f20808caf629417d7427401282119f3282212475
Jay McKerracher <jaymckerracher@gmail.com>
2026-02-22 19:46:29 +0000

    add route for user dashboard

--- server.js
+++ server.js
@@ -7,8 +7,12 @@ const port = 3000;
 app.use(express.static(path.join(__dirname, "public/pages")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile("index.html");
 });
+
+app.get("/user-dashboard", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/pages", "user_dashboard.html"));
+})
 
 app.listen(port, () => {
   console.log("We are running!");
